da87e3d0cc08663e9e98fcc52a2852e3cc6135ce
zidniryi123 <zidniryi123@gmail.com>
2023-04-09 22:21:28 +0700

    add unit test

--- test/flutter_awesome_tooltip_test.dart
+++ test/flutter_awesome_tooltip_test.dart
@@ -1,12 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_awesome_tooltip/flutter_awesome_tooltip.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_awesome_tooltip/flutter_awesome_tooltip.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('LibraryTooltip should show a tooltip on tap',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: FlutterAwesomeTooltip(
+            message: 'This is a tooltip message',
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // The tooltip should not be visible initially
+    expect(find.byType(Tooltip), findsNothing);
+
+    // Tap on the child widget to show the tooltip
+    await tester.tap(find.byType(Container));
+    await tester.pumpAndSettle();
+
+    // The tooltip should now be visible
+    expect(find.byType(Tooltip), findsOneWidget);
+
+    // Dismiss the tooltip by tapping on the screen
+    await tester.tap(find.byType(Material));
+    await tester.pumpAndSettle();
+
+    // The tooltip should be hidden again
+    expect(find.byType(Tooltip), findsNothing);
   });
 }
